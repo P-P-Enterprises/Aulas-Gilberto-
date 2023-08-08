85ef0223220f226661586b2fdab3c4173ffa3348
Samuel Lemos <107198491+invitaman@users.noreply.github.com>
2023-08-08 10:50:04 -0300

    Add files via upload

---
+++ Exercicios 1js/script.js
@@ -0,0 +1,8 @@
+const objexe= {
+    Nome: 'Paulo',
+    Nota: '95',
+    Matricula: '15484649',
+
+  };
+
+  console.log(" O aluno : "+ objexe.Nome +", De matricula : "+ objexe.Matricula + ", Possui nota : " +objexe.Nota);
